0b8b2ec72176a212c4582ab5669332f4205e2576
ZachShirikjian <zshirikjian@gmail.com>
2024-07-23 15:47:21 -0400

    SCENE MADE

---
+++ RushOfGold/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //VARIABLES//
+    public int timeRemaining = 60; //The Time remaining within a match 
+    
+    //REFERENCES//
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RushOfGold/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //VARIABLES//
+    public float speed; 
+    
+    //REFERENCES//
+    public Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
