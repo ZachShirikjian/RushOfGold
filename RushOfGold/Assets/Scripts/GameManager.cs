ffb3d610e1a190bcdf135b28b79f33e16667657b
ZachShirikjian <zshirikjian@gmail.com>
2024-07-24 11:08:23 -0400

    Updated Coin & Timer UI

--- RushOfGold/Assets/Scripts/GameManager.cs
+++ RushOfGold/Assets/Scripts/GameManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     //VARIABLES//
     public int timeRemaining = 60; //The Time remaining within a match 
+    public int p1Lives = 3; //Number of Lives Player 1 has
+    public int p2Lives = 3; //Number of Lives Player 2 has 
+    public int p1Coins = 0; //Amount of Coins Player 1 has 
+    public int p2Coins = 0; //Amount of Coins Player 2 has 
     
     //REFERENCES//
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //Resets the Lives & Coins for P1 & P2, and the timer for each new level.
+    public void ResetValues()
+    {
+        p1Lives = 3;
+        p2Lives = 3;
+        timeRemaining = 60;
+        p1Coins = 0;
+        p2Coins = 0; 
     }
 }
