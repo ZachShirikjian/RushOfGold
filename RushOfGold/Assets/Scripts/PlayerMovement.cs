dd1310ab2e704e2efa6a96952b2bf49939e78080
Elijah Baptiste <147116516+elijahbaptiste@users.noreply.github.com>
2024-07-24 08:40:40 -0700

    added basic movement control for both players

--- RushOfGold/Assets/Scripts/PlayerMovement.cs
+++ RushOfGold/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     
     //REFERENCES//
     public Rigidbody2D rb2D;
+    public Rigidbody2D rb2D2;
+    public float MoveSpeed = 5f;
+    public float JumpPower;
+    private Vector2 p1x, p2x;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,54 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Sidways movement control for player 1
+        if (Input.GetKey(KeyCode.A))
+        {
+            p1x = new Vector2(-1f, 0f);
+            rb2D.AddForce(p1x * MoveSpeed );
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            p1x = new Vector2(1f, 0f);
+            rb2D.AddForce(p1x * MoveSpeed);
+        }
+
+        //Sideways player control for player 2 
+        if (Input.GetKey(KeyCode.J))
+        {
+            p2x = new Vector2(-1f, 0f); 
+            rb2D2.AddForce(p1x * MoveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            p2x = new Vector2(1f, 0f);
+            rb2D2.AddForce(p1x * MoveSpeed);
+        }
+
+
+
+        //Jump and Attack controls for Player 1
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+
+        }
+
+        //Jump and Attack controls for Player 2
+       // if (Input.GetKeyDown("a"))
+        {
+
+        }
+       // if (Input.GetKeyDown("d"))
+        {
+
+        }
     }
 }
