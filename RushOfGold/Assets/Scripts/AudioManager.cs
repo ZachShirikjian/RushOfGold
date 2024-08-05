a7f922c6a8a6817efa4e5ac615f6670dd83e8913
ZachShirikjian <zshirikjian@gmail.com>
2024-08-05 12:01:24 -0400

    AudioStarted

---
+++ RushOfGold/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //REFERENCES TO SFX USED IN OUR GAME//
+    public AudioClip coinCollect; 
+
+    //REFERENCES TO MUSIC USED IN OUR GAME//
+    public AudioClip level1Theme;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
